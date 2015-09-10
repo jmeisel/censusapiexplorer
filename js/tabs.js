daa5a0738a74d3a30f09302e1f3104ea07cfef9f
Jeff Meisel <jeffreymeisel@gmail.com>
2015-09-10 11:43:19 -0500

    Initial Commit
    * Basic shell with table
    * Example of interactively exploring APIs

---
+++ js/tabs.js
@@ -0,0 +1,102 @@
+    var data = [
+  {
+    "type":"CitySDK",
+    "description1":"The CitySDK takes the vast amount of Census data, and focuses on making the most commonly used datasets as easy to use as possible for software developers using Javascript.",
+    "description2":"If you are using Geographies, and data from the American Community Survery (i.e. People, Economy, Housing) than the CitySDK is a good place to start. ",
+    "description3":"If you want direct access to the APIs, or are using tools other than Javascript, you can still use the Census API directly instead of the CitySDK. ",
+    "gettingstarted":"View the CitySDK Getting Started Guide",
+    "advanced1":"",
+    "advanced2":"",
+    "advanced3":""
+  },
+  {
+    "type":"Geography",
+    "description1":"You can access shape files, which are boundaries for common geographic entities like states, counties, cities, and more.  This can help provide context when building a mapping app.  ",
+    "description2":"You can also geocode a location to another format, for example, pass in a mailing address and get back a lat/lon pair that corresponds to that address.",
+    "description3":"The CitySDK makes it easy to get these geographic boundaries in GeoJSON formats which can easily be consumed by mapping tools like Google Maps and Leaflet JS.  ",
+    "gettingstarted":"View the CitySDK Docs related to Geography:",
+    "advanced1":"View the TigerWeb API",
+    "advanced2":"View the Geocoder API",
+    "advanced3":""
+  },
+  {
+    "type":"People",
+    "description1":"You can access shape files, which are boundaries for common geographic entities like states, counties, cities, and more.  This can help provide context when building a mapping app.  ",
+    "description2":"You can also geocode a location to another format, for example, pass in a mailing address and get back a lat/lon pair that corresponds to that address.",
+    "description3":"The CitySDK makes it easy to get these geographic boundaries in GeoJSON formats which can easily be consumed by mapping tools like Google Maps and Leaflet JS.  ",
+    "gettingstarted":"View the CitySDK Docs related to Geography:",
+    "advanced1":"View the TigerWeb API",
+    "advanced2":"View the Geocoder API",
+    "advanced3":""
+  },
+  {
+    "type":"Economy",
+    "description1":"You can access shape files, which are boundaries for common geographic entities like states, counties, cities, and more.  This can help provide context when building a mapping app.  ",
+    "description2":"You can also geocode a location to another format, for example, pass in a mailing address and get back a lat/lon pair that corresponds to that address.",
+    "description3":"The CitySDK makes it easy to get these geographic boundaries in GeoJSON formats which can easily be consumed by mapping tools like Google Maps and Leaflet JS.  ",
+    "gettingstarted":"View the CitySDK Docs related to Geography:",
+    "advanced1":"View the TigerWeb API",
+    "advanced2":"View the Geocoder API",
+    "advanced3":""
+  },
+  {
+    "type":"Manufacturing",
+    "description1":"You can access shape files, which are boundaries for common geographic entities like states, counties, cities, and more.  This can help provide context when building a mapping app.  ",
+    "description2":"You can also geocode a location to another format, for example, pass in a mailing address and get back a lat/lon pair that corresponds to that address.",
+    "description3":"The CitySDK makes it easy to get these geographic boundaries in GeoJSON formats which can easily be consumed by mapping tools like Google Maps and Leaflet JS.  ",
+    "gettingstarted":"View the CitySDK Docs related to Geography:",
+    "advanced1":"View the TigerWeb API",
+    "advanced2":"View the Geocoder API",
+    "advanced3":""
+  },
+  {
+    "type":"Housing",
+    "description1":"You can access shape files, which are boundaries for common geographic entities like states, counties, cities, and more.  This can help provide context when building a mapping app.  ",
+    "description2":"You can also geocode a location to another format, for example, pass in a mailing address and get back a lat/lon pair that corresponds to that address.",
+    "description3":"The CitySDK makes it easy to get these geographic boundaries in GeoJSON formats which can easily be consumed by mapping tools like Google Maps and Leaflet JS.  ",
+    "gettingstarted":"View the CitySDK Docs related to Geography:",
+    "advanced1":"View the TigerWeb API",
+    "advanced2":"View the Geocoder API",
+    "advanced3":""
+  },
+  {
+    "type":"Commuting",
+    "description1":"You can access shape files, which are boundaries for common geographic entities like states, counties, cities, and more.  This can help provide context when building a mapping app.  ",
+    "description2":"You can also geocode a location to another format, for example, pass in a mailing address and get back a lat/lon pair that corresponds to that address.",
+    "description3":"The CitySDK makes it easy to get these geographic boundaries in GeoJSON formats which can easily be consumed by mapping tools like Google Maps and Leaflet JS.  ",
+    "gettingstarted":"View the CitySDK Docs related to Geography:",
+    "advanced1":"View the TigerWeb API",
+    "advanced2":"View the Geocoder API",
+    "advanced3":""
+  },
+  {
+    "type":"Education",
+    "description1":"You can access shape files, which are boundaries for common geographic entities like states, counties, cities, and more.  This can help provide context when building a mapping app.  ",
+    "description2":"You can also geocode a location to another format, for example, pass in a mailing address and get back a lat/lon pair that corresponds to that address.",
+    "description3":"The CitySDK makes it easy to get these geographic boundaries in GeoJSON formats which can easily be consumed by mapping tools like Google Maps and Leaflet JS.  ",
+    "gettingstarted":"View the CitySDK Docs related to Geography:",
+    "advanced1":"View the TigerWeb API",
+    "advanced2":"View the Geocoder API",
+    "advanced3":""
+  },
+  {
+    "type":"Medical",
+    "description1":"You can access shape files, which are boundaries for common geographic entities like states, counties, cities, and more.  This can help provide context when building a mapping app.  ",
+    "description2":"You can also geocode a location to another format, for example, pass in a mailing address and get back a lat/lon pair that corresponds to that address.",
+    "description3":"The CitySDK makes it easy to get these geographic boundaries in GeoJSON formats which can easily be consumed by mapping tools like Google Maps and Leaflet JS.  ",
+    "gettingstarted":"View the CitySDK Docs related to Geography:",
+    "advanced1":"View the TigerWeb API",
+    "advanced2":"View the Geocoder API",
+    "advanced3":""
+  },
+  {
+    "type":"International",
+    "description1":"You can access shape files, which are boundaries for common geographic entities like states, counties, cities, and more.  This can help provide context when building a mapping app.  ",
+    "description2":"You can also geocode a location to another format, for example, pass in a mailing address and get back a lat/lon pair that corresponds to that address.",
+    "description3":"The CitySDK makes it easy to get these geographic boundaries in GeoJSON formats which can easily be consumed by mapping tools like Google Maps and Leaflet JS.  ",
+    "gettingstarted":"View the CitySDK Docs related to Geography:",
+    "advanced1":"View the TigerWeb API",
+    "advanced2":"View the Geocoder API",
+    "advanced3":""
+  }
+]
