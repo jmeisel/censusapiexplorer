cbf53ace50d48ea8972ccd05db24cc0da55aeeec
Jeff Meisel <jeffreymeisel@gmail.com>
2015-09-11 09:24:49 -0400

    Added popovers

--- js/tabs.js
+++ js/tabs.js
@@ -4,99 +4,69 @@
     "description1":"The CitySDK takes the vast amount of Census data, and focuses on making the most commonly used datasets as easy to use as possible for software developers using Javascript.",
     "description2":"If you are using Geographies, and data from the American Community Survery (i.e. People, Economy, Housing) than the CitySDK is a good place to start. ",
     "description3":"If you want direct access to the APIs, or are using tools other than Javascript, you can still use the Census API directly instead of the CitySDK. ",
-    "gettingstarted":"View the CitySDK Getting Started Guide",
-    "advanced1":"",
-    "advanced2":"",
-    "advanced3":""
+    "gettingstarted":"View the CitySDK Getting Started Guide"
   },
   {
     "type":"Geography",
     "description1":"You can access shape files, which are boundaries for common geographic entities like states, counties, cities, and more.  This can help provide context when building a mapping app.  ",
     "description2":"You can also geocode a location to another format, for example, pass in a mailing address and get back a lat/lon pair that corresponds to that address.",
     "description3":"The CitySDK makes it easy to get these geographic boundaries in GeoJSON formats which can easily be consumed by mapping tools like Google Maps and Leaflet JS.  ",
-    "gettingstarted":"View the CitySDK Docs related to Geography:",
-    "advanced1":"View the TigerWeb API",
-    "advanced2":"View the Geocoder API",
-    "advanced3":""
+    "gettingstarted":"View the CitySDK Docs related to Geography:"
   },
   {
     "type":"People",
-    "description1":"You can access shape files, which are boundaries for common geographic entities like states, counties, cities, and more.  This can help provide context when building a mapping app.  ",
-    "description2":"You can also geocode a location to another format, for example, pass in a mailing address and get back a lat/lon pair that corresponds to that address.",
-    "description3":"The CitySDK makes it easy to get these geographic boundaries in GeoJSON formats which can easily be consumed by mapping tools like Google Maps and Leaflet JS.  ",
-    "gettingstarted":"View the CitySDK Docs related to Geography:",
-    "advanced1":"View the TigerWeb API",
-    "advanced2":"View the Geocoder API",
-    "advanced3":""
+    "description1":"All population related censuses and surveys contain data on people and basic demographics. ",
+    "description2":"For population counts only, the decennial census or the most recent population estimates are the best source. ",
+    "description3":"The American Community Survey provides multiple pieces of demographic data such as population by age, race, sex, Hispanic Origin crosstabulated  with a variety of other variables.",
+    "gettingstarted":"CitySDK/Census API"
   },
   {
     "type":"Economy",
-    "description1":"You can access shape files, which are boundaries for common geographic entities like states, counties, cities, and more.  This can help provide context when building a mapping app.  ",
-    "description2":"You can also geocode a location to another format, for example, pass in a mailing address and get back a lat/lon pair that corresponds to that address.",
-    "description3":"The CitySDK makes it easy to get these geographic boundaries in GeoJSON formats which can easily be consumed by mapping tools like Google Maps and Leaflet JS.  ",
-    "gettingstarted":"View the CitySDK Docs related to Geography:",
-    "advanced1":"View the TigerWeb API",
-    "advanced2":"View the Geocoder API",
-    "advanced3":""
+    "description1":"Many Census surveys cotain economic data. A good place to start is the Economic Census which provides data by industry every 5 years.",
+    "description2":"For more recent economic data, use County Business Patterns to identify the number of establishments and paid employees by sector.",
+    "description3":"For economic data relating to people, such as income and employment, the American Community Survey is your best bet.",
+    "gettingstarted":"CitySDK/Census API"
   },
   {
     "type":"Manufacturing",
-    "description1":"You can access shape files, which are boundaries for common geographic entities like states, counties, cities, and more.  This can help provide context when building a mapping app.  ",
-    "description2":"You can also geocode a location to another format, for example, pass in a mailing address and get back a lat/lon pair that corresponds to that address.",
-    "description3":"The CitySDK makes it easy to get these geographic boundaries in GeoJSON formats which can easily be consumed by mapping tools like Google Maps and Leaflet JS.  ",
-    "gettingstarted":"View the CitySDK Docs related to Geography:",
-    "advanced1":"View the TigerWeb API",
-    "advanced2":"View the Geocoder API",
-    "advanced3":""
+    "description1":"Manufacturing data is available from several Census sources.",
+    "description2":"Your best bet for local level data on the manufacturing industry is the Economic Census and County Business Patterns.",
+    "description3":"The Annual Survey of Manufacturers contains data for manufacturing establishments, but only at the state level and above.",
+    "gettingstarted":"Census API"
   },
   {
     "type":"Housing",
-    "description1":"You can access shape files, which are boundaries for common geographic entities like states, counties, cities, and more.  This can help provide context when building a mapping app.  ",
-    "description2":"You can also geocode a location to another format, for example, pass in a mailing address and get back a lat/lon pair that corresponds to that address.",
-    "description3":"The CitySDK makes it easy to get these geographic boundaries in GeoJSON formats which can easily be consumed by mapping tools like Google Maps and Leaflet JS.  ",
-    "gettingstarted":"View the CitySDK Docs related to Geography:",
-    "advanced1":"View the TigerWeb API",
-    "advanced2":"View the Geocoder API",
-    "advanced3":""
+    "description1":"Many Census surveys and censuses contain data on hosuing but for the most current data in between censuses, your best bet is the American Community Survey.",
+    "description2":"Some examples of the data: Year built, Owner vs. renter occupied, type of heating, how many rooms, bedrooms, etc.",
+    "description3":"Information on housing costs and regular living expenses such as rent, mortgage, taxes and utilities is also included.",
+    "gettingstarted":"CitySDK"
   },
   {
     "type":"Commuting",
-    "description1":"You can access shape files, which are boundaries for common geographic entities like states, counties, cities, and more.  This can help provide context when building a mapping app.  ",
-    "description2":"You can also geocode a location to another format, for example, pass in a mailing address and get back a lat/lon pair that corresponds to that address.",
-    "description3":"The CitySDK makes it easy to get these geographic boundaries in GeoJSON formats which can easily be consumed by mapping tools like Google Maps and Leaflet JS.  ",
-    "gettingstarted":"View the CitySDK Docs related to Geography:",
-    "advanced1":"View the TigerWeb API",
-    "advanced2":"View the Geocoder API",
-    "advanced3":""
+    "description1":"Data on commuting come from the American Community Survey.",
+    "description2":"This data answers questions such as: How do people get to work?, Do they carpool?, What is the average commuting time?",
+    "description3":"Uses: improvements to roads/highways, developing transportation services, creating emergency response strategies",
+    "gettingstarted":"CitySDK"
   },
   {
     "type":"Education",
-    "description1":"You can access shape files, which are boundaries for common geographic entities like states, counties, cities, and more.  This can help provide context when building a mapping app.  ",
-    "description2":"You can also geocode a location to another format, for example, pass in a mailing address and get back a lat/lon pair that corresponds to that address.",
-    "description3":"The CitySDK makes it easy to get these geographic boundaries in GeoJSON formats which can easily be consumed by mapping tools like Google Maps and Leaflet JS.  ",
-    "gettingstarted":"View the CitySDK Docs related to Geography:",
-    "advanced1":"View the TigerWeb API",
-    "advanced2":"View the Geocoder API",
-    "advanced3":""
+    "description1":"Data on education come largely from the American Community Survey.",
+    "description2":"Examples of the data include: school enrollment, educational attainment, including categories of high school diploma or equiv., a bachelor's degree or higher, etc.",
+    "description3":"Interesting cross-tabulations with education- median earnings and poverty rates by education level.",
+    "gettingstarted":"CitySDK"
   },
   {
-    "type":"Medical",
-    "description1":"You can access shape files, which are boundaries for common geographic entities like states, counties, cities, and more.  This can help provide context when building a mapping app.  ",
-    "description2":"You can also geocode a location to another format, for example, pass in a mailing address and get back a lat/lon pair that corresponds to that address.",
-    "description3":"The CitySDK makes it easy to get these geographic boundaries in GeoJSON formats which can easily be consumed by mapping tools like Google Maps and Leaflet JS.  ",
-    "gettingstarted":"View the CitySDK Docs related to Geography:",
-    "advanced1":"View the TigerWeb API",
-    "advanced2":"View the Geocoder API",
-    "advanced3":""
+    "type":"Health",
+    "description1":"Health insurance and disability data come from the American Community Survey.",
+    "description2":"Health insurance coverage- are people covered or not? If they are covered, what type of insurance? ",
+    "description3":"Disability data speak to a person's difficulty with specific daily tasks: seeing or hearing, walking or climbing stairs, dressing or bathing.",
+    "gettingstarted":"CitySDK"
   },
   {
     "type":"International",
-    "description1":"You can access shape files, which are boundaries for common geographic entities like states, counties, cities, and more.  This can help provide context when building a mapping app.  ",
-    "description2":"You can also geocode a location to another format, for example, pass in a mailing address and get back a lat/lon pair that corresponds to that address.",
-    "description3":"The CitySDK makes it easy to get these geographic boundaries in GeoJSON formats which can easily be consumed by mapping tools like Google Maps and Leaflet JS.  ",
-    "gettingstarted":"View the CitySDK Docs related to Geography:",
-    "advanced1":"View the TigerWeb API",
-    "advanced2":"View the Geocoder API",
-    "advanced3":""
+    "description1":"International data fall into two large categories: demographic data and trade.",
+    "description2":"For demographic data on countriesn with 5k or more, the International Data Base is the source to use.",
+    "description3":"Interested in annual import and export values? Use International Trade.",
+    "gettingstarted":"Census API"
   }
 ]
